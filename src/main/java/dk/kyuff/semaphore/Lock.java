f22b95765a122e5f3fdf7f0c8de1ec5c88d2ae3d
Søren Kyndi-Wiuff <soren@sxl.dk>
2014-11-22 07:13:03 +0100

    Added Maven structure

---
+++ src/main/java/dk/kyuff/semaphore/Lock.java
@@ -0,0 +1,10 @@
+package dk.kyuff.semaphore;
+
+/**
+ * User: swi
+ * Date: 22/11/14
+ * Time: 07.11
+ */
+public enum Lock {
+}
+                               ‡
---
+++ src/test/java/dk/kyuff/semaphore/LockTest.java
@@ -0,0 +1,9 @@
+package dk.kyuff.semaphore;
+
+/**
+ * User: swi
+ * Date: 22/11/14
+ * Time: 07.12
+ */
+public class LockTest {
+}
